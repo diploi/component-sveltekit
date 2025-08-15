39612f8a5866d21a32ac0536feefeacd2b3f4564
giaongo <83873333+giaongo@users.noreply.github.com>
2025-08-15 11:43:03 +0300

    chore: add .diploi.me to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    allowedHosts: ['.diploi.app'],
+    allowedHosts: ['.diploi.app', '.diploi.me'],
     // @ts-ignore
     port: process.env.PORT,
   },
